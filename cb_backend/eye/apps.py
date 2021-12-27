17541aa99faed0260528e2bcb11702bbfe603ffe
leonardo orozco <leonardoorozcop@gmail.com>
2021-12-27 12:28:17 -0500

    initial base eye app with models

---
+++ cb_backend/eye/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class EyeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "cb_backend.eye"
+    verbose_name = _("Eyes")
---
+++ cb_backend/eye/models.py
@@ -0,0 +1,71 @@
+import uuid
+
+from django.conf import settings
+from django.db import models
+
+
+class Application(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    token = models.UUIDField(default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='applications', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Session(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    application = models.ForeignKey(Application, related_name='sessions', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class CategoryEvent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(CategoryEvent, related_name='events', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    validators = models.JSONField()
+
+    def __str__(self):
+        return str(self.id)
+
+
+class EventSessionStatus(models.TextChoices):
+    PENDING = 'PENDING', 'Pending'
+    VALIDATED = 'VALIDATED', 'Validated'
+    REJECTED = 'REJECTED', 'Rejected'
+
+
+class EventSession(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    session = models.ForeignKey(Session, related_name='events', on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, related_name='events', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    payload = models.JSONField()
+    timestamp = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=EventSessionStatus.choices, default=EventSessionStatus.PENDING)
+    payload_error = models.JSONField(null=True)
+
+    def __str__(self):
+        return str(self.id)
